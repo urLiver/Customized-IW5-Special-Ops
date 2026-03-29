dd1a040d32953110e681c932954c61e3e88d6f6e
urLiver <>
2026-03-29 12:56:37 +0200

    Initial commit

---
+++ build_scripts/build code_post_gfx.bat
@@ -0,0 +1,5 @@
+@echo off
+
+linker.exe --load "OAT\zone\english\code_post_gfx.ff" code_post_gfx_patch
+
+pause
---
+++ build_scripts/build patch_specialops.bat
@@ -0,0 +1,5 @@
+@echo off
+
+linker.exe --load "OAT\zone\english\patch_specialops.ff" patch_specialops_patch
+
+pause
