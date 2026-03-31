5e9f7ecf2ac5767030e606a35631ae21febbe57f
urLiver <>
2026-03-31 22:53:46 +0200

    Added chance on hyena model instead of dog model

--- fastfiles/build patch_survival.bat
+++ fastfiles/build patch_survival.bat
@@ -1,6 +1,6 @@
 @echo off
 
-linker.exe --load "OAT\zone\english\aizombies_misc.ff" --load "OAT\zone\english\iw4_weapons.ff" --load "OAT\zone\english\common_mp.ff" --load "OAT\zone\english\common.ff" --load "OAT\zone\english\patch_survival.ff" --load "OAT\zone\english\sp_berlin.ff" --load "OAT\zone\english\castle.ff" --load "OAT\zone\english\ny_harbor.ff" --load "OAT\zone\english\so_timetrial_london.ff" --load "OAT\zone\english\so_nyse_ny_manhattan.ff" --load "OAT\zone\english\so_rescue_hijack.ff" --load "OAT\zone\english\rescue_2.ff" --load "OAT\zone\english\prague_escape.ff" --load "OAT\zone\english\paris_ac130.ff" --load "OAT\zone\english\mp_alpha.ff" --load "OAT\zone\english\mp_dome.ff" patch_survival_patch
+linker.exe --load "OAT\zone\english\warlord.ff" --load "OAT\zone\english\aizombies_misc.ff" --load "OAT\zone\english\iw4_weapons.ff" --load "OAT\zone\english\common_mp.ff" --load "OAT\zone\english\common.ff" --load "OAT\zone\english\patch_survival.ff" --load "OAT\zone\english\sp_berlin.ff" --load "OAT\zone\english\castle.ff" --load "OAT\zone\english\ny_harbor.ff" --load "OAT\zone\english\so_timetrial_london.ff" --load "OAT\zone\english\so_nyse_ny_manhattan.ff" --load "OAT\zone\english\so_rescue_hijack.ff" --load "OAT\zone\english\rescue_2.ff" --load "OAT\zone\english\prague_escape.ff" --load "OAT\zone\english\paris_ac130.ff" --load "OAT\zone\english\mp_alpha.ff" --load "OAT\zone\english\mp_dome.ff" patch_survival_patch
 
 pause
 
